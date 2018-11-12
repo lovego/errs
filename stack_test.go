1a76b922dc5af8c0edaf365f2d41f6738e9b1011
bughou <applejava@qq.com>
2018-11-12 14:28:50 +0800

    fix unit test for 1.8 and 1.9

--- stack_test.go
+++ stack_test.go
@@ -11,7 +11,8 @@ func ExampleStack() {
 	re := regexp.MustCompile(`^runtime\.Callers
 	/.+/src/runtime/extern\.go:2(12|24) \(0x[a-z0-9]+\)
 github.com/lovego/errs.Stack
-	/.+/src/github\.com/lovego/errs/(stack\.go:13|_test/_obj_test/stack\.go:14) \(0x[a-z0-9]+\)
+	(/.+/src/github\.com/lovego/errs/stack\.go:13|` +
+		`github\.com/lovego/errs/_test/_obj_test/stack\.go:14) \(0x[a-z0-9]+\)
 github.com/lovego/errs.ExampleStack
 	/.+/src/github.com/lovego/errs/stack_test\.go:10 \(0x[a-z0-9]+\)
 `)
@@ -27,7 +28,7 @@ func ExampleWithStack() {
 	stack := WithStack(Trace(err))
 	re := regexp.MustCompile(`the error
 github.com/lovego/errs.ExampleWithStack
-	/.+/src/github.com/lovego/errs/stack_test\.go:27 \(0x[a-z0-9]+\)
+	/.+/src/github.com/lovego/errs/stack_test\.go:28 \(0x[a-z0-9]+\)
 `)
 	if !re.MatchString(stack) {
 		fmt.Println(stack)
